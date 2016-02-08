8438a278c52878b102de30cae1b3b94b11348f30
Trystan Lea <trystan.lea@gmail.com>
2016-02-08 15:04:26 +0000

    auto updating console windows

--- edit.php
+++ edit.php
@@ -1,10 +1,40 @@
 <?php global $path; ?>
 
+<style>
+pre {
+    width:100%;
+    height:600px;
+    
+    
+    margin:0px;
+    padding:0px;
+    font-size:16px;
+    color:#fff;
+    background-color:#300a24;
+    overflow: scroll;
+    overflow-x: hidden;
+    
+    font-size:16px;
+}
+#emoncms-console-log {
+    padding-left:20px;
+    padding-top:20px;
+}
+#emonhub-console-log {
+    padding-left:20px;
+    padding-top:20px;
+}
+
+</style>
+
 <br>
 <h3>EmonHub Config Editor</h3>
-<button id="show-emoncmslogview" style="float:right">emoncms.log view</button>
-<button id="show-emonhublogview" style="float:right">emonhub.log view</button>
-<button id="show-editor" style="float:right">Editor</button>
+
+<div class="input-prepend input-append" style="float:right">
+    <button class="btn" id="show-editor">Editor</button>
+    <button class="btn" id="show-emonhublogview">emonhub.log view</button>
+    <button class="btn" id="show-emoncmslogview">emoncms.log view</button>
+</div>
 
 <div id="editor">
     <button class="save">Save</button><br><br>
@@ -13,15 +43,25 @@
 </div>
 
 <div id="emoncmslogview" style="display:none">
-    <br><br>
-    <pre id="emoncmslogviewpre"></pre>
-    <button class="emoncmslogrefresh">Refresh</button>
+
+    <div class="input-prepend input-append">
+        <span class="add-on">Auto update</span>
+        <button class="btn autoupdate-toggle">ON</button>
+    </div>
+    
+    <pre id="emoncmslogviewpre"><div id="emoncms-console-log"></div></pre><br>
+
 </div>
 
 <div id="emonhublogview" style="display:none">
-    <br><br>
-    <pre id="emonhublogviewpre"></pre>
-    <button class="emonhublogrefresh">Refresh</button>
+
+    <div class="input-prepend input-append">
+        <span class="add-on">Auto update</span>
+        <button class="btn autoupdate-toggle">ON</button>
+    </div>
+    
+    <pre id="emonhublogviewpre"><div id="emonhub-console-log"></div></pre><br>
+
 </div>
 
 <script>
@@ -29,6 +69,10 @@
 var path = "<?php echo $path; ?>";
 
 var config = "";
+
+var emonhublog_updater = false;
+var emoncmslog_updater = false;
+var autoupdate = false;
 
 $.ajax({ 
     url: path+"config/get", 
@@ -45,21 +89,25 @@ $(".save").click(function(){
     $.ajax({ type: "POST", url: path+"config/set", data: "config="+config, async: false, success: function(data){console.log(data);} });
 });
 
-$(".emonhublogrefresh").click(function(){
-    emonhublog_refresh();
-});
-
-$(".emoncmslogrefresh").click(function(){
-    emoncmslog_refresh();
+$(".autoupdate-toggle").click(function(){
+    if (autoupdate==true) {
+        autoupdate = false;
+        disable_autoupdate();
+    } else {
+        autoupdate = true;
+        enable_autoupdate();
+    }
 });
 
 $("#show-editor").click(function(){
     $("#editor").show();
     $("#emonhublogview").hide();
     $("#emoncmslogview").hide();
+    disable_autoupdate();
 });
 
 $("#show-emonhublogview").click(function(){
+    if (!autoupdate) enable_autoupdate();
     emonhublog_refresh();
     $("#emonhublogview").show();
     $("#emoncmslogview").hide();
@@ -67,19 +115,34 @@ $("#show-emonhublogview").click(function(){
 });
 
 $("#show-emoncmslogview").click(function(){
+    if (!autoupdate) enable_autoupdate();
     emoncmslog_refresh();
     $("#emonhublogview").hide();
     $("#emoncmslogview").show();
     $("#editor").hide();
 });
 
+function enable_autoupdate() {
+    autoupdate = true;
+    $(".autoupdate-toggle").html("ON");
+    emonhublog_updater = setInterval(emonhublog_refresh,1000);
+    emoncmslog_updater = setInterval(emoncmslog_refresh,1000);
+}
+
+function disable_autoupdate() {
+    autoupdate = false;
+    $(".autoupdate-toggle").html("OFF");
+    clearInterval(emonhublog_updater);
+    clearInterval(emoncmslog_updater);
+}
+
 function emonhublog_refresh()
 {
     $.ajax({ 
         url: path+"config/getemonhublog", 
-        dataType: 'text', async: false, 
+        dataType: 'text', async: true, 
         success: function(data) {
-            $("#emonhublogviewpre").html(data);
+            $("#emonhub-console-log").html(data+"\n\n");
         } 
     });
 }
@@ -88,9 +151,9 @@ function emoncmslog_refresh()
 {
     $.ajax({ 
         url: path+"config/getemoncmslog", 
-        dataType: 'text', async: false, 
+        dataType: 'text', async: true, 
         success: function(data) {
-            $("#emoncmslogviewpre").html(data);
+            $("#emoncms-console-log").html(data+"\n\n");
         } 
     });
 }
