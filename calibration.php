28a5887f5155e3d183d29edf622e8e348cd3a46d
TrystanLea <trystan.lea@gmail.com>
2018-12-27 14:24:48 +0000

    updating last time & value

--- calibration.php
+++ calibration.php
@@ -58,6 +58,7 @@ select { margin:0px; width:300px; }
             <th>Calibration</th>
             <th>Phase Shift</th>
             <th>Value</th>
+            <th>Time</th>
           </tr>
 
           <tr v-for="(input,index) in node.rx.unitless" v-if="input=='v'">
@@ -69,12 +70,13 @@ select { margin:0px; width:300px; }
               <button class="btn">+</button>
             </div></td>
             <td></td>
-            <td><div style="color:#00aa00">245V</div></td>
+            <td><span v-if="typeof live[node.nodename]!=='undefined'" v-html="list_format_value(live[node.nodename][node.rx.names[index]].value)"></span>{{ node.rx.units[index] }}</td>
+            <td><span v-if="typeof live[node.nodename]!=='undefined'" v-html="list_format_updated(live[node.nodename][node.rx.names[index]].time)"></span></td>
           </tr>
           
-          <tr v-for="(input,index) in conf.nodes[nodeid].rx.unitless" v-if="input=='rp'">
-            <td>{{ conf.nodes[nodeid].rx.names[index] }}</td>
-            <td><select><option>SCT-013-000: 2000 turns, 22R burden</option></select></td>
+          <tr v-for="(input,index) in node.rx.unitless" v-if="input=='rp'">
+            <td>{{ node.rx.names[index] }}</td>
+            <td><select><option>SCT-013-000: 2000 turns, 22R burden</option><option>SCT-013-000: 2000 turns, 100R burden</option></select></td>
             <td><div class="input-prepend input-append">
               <button class="btn">-</button>
               <input type="text" style="width:70px" v-model="node.rx.icals[index]" />
@@ -85,7 +87,8 @@ select { margin:0px; width:300px; }
               <input type="text" style="width:70px" v-model="node.rx.phase_shifts[index]" />
               <button class="btn">+</button>
             </div></td>
-            <td><div style="color:#00aa00">100W</div></td>
+            <td><span v-if="typeof live[node.nodename]!=='undefined'" v-html="list_format_value(live[node.nodename][node.rx.names[index]].value)"></span>{{ node.rx.units[index] }}</td>
+            <td><span v-if="typeof live[node.nodename]!=='undefined'" v-html="list_format_updated(live[node.nodename][node.rx.names[index]].time)"></span></td>
           </tr>       
         </table>
       </div>
@@ -114,7 +117,20 @@ console.log(JSON.parse(JSON.stringify(conf)));
 
 var app = new Vue({
   el: '#conf',
-  data: { conf: conf }
+  data: { conf: conf, live: "hello" },
+  filters: {
+    dp2: function(value) {
+      return value.toFixed(2);
+    }
+  },
+  methods: {
+    list_format_updated: function(value) {
+      return list_format_updated(value)
+    },
+    list_format_value: function(value) {
+      return list_format_value(value)
+    }
+  }
 });
 
 $("#conf").on('click',".section-heading",function(){
@@ -129,5 +145,56 @@ $("#conf").on("change","input",function() {
   // --- 
   }});
 });
+
+update();
+setInterval(update,5000);
+function update(){
+    $.ajax({ type: "GET", url: path+"input/get", async: false, success: function(result){ 
+        app.live = result;
+    }});
+}
+
+// -------------------------------------------------------------------------
+
+function list_format_updated(time) {
+    time = time * 1000;
+    var servertime = new Date().getTime(); // - table.timeServerLocalOffset;
+    var update = new Date(time).getTime();
+    
+    var secs = (servertime - update) / 1000;
+    var mins = secs / 60;
+    var hour = secs / 3600;
+    var day = hour / 24;
+    
+    var updated = secs.toFixed(0) + "s";
+    if (update == 0 || !$.isNumeric(secs)) updated = "n/a";
+    else if (secs < 0) updated = secs.toFixed(0) + "s";
+    // update time ahead of server date is signal of slow network
+    else if (secs.toFixed(0) == 0) updated = "now";
+    else if (day > 7) updated = "inactive";
+    else if (day > 2) updated = day.toFixed(1) + " days";
+    else if (hour > 2) updated = hour.toFixed(0) + " hrs";
+    else if (secs > 180) updated = mins.toFixed(0) + " mins";
+    
+    secs = Math.abs(secs);
+    var color = "rgb(255,0,0)";
+    if (secs < 25) color = "rgb(50,200,50)";
+    else if (secs < 60) color = "rgb(240,180,20)";
+    else if (secs < 3600 * 2) color = "rgb(255,125,20)";
+    
+    return "<span style='color:" + color + ";'>" + updated + "</span>";
+}
+
+function list_format_value(value) {
+    if (value == null) return "NULL";
+    value = parseFloat(value);
+    if (value >= 1000) value = parseFloat(value.toFixed(0));
+    else if (value >= 100) value = parseFloat(value.toFixed(1));
+    else if (value >= 10) value = parseFloat(value.toFixed(2));
+    else if (value <= -1000) value = parseFloat(value.toFixed(0));
+    else if (value <= -100) value = parseFloat(value.toFixed(1));
+    else if (value < 10) value = parseFloat(value.toFixed(2));
+    return value;
+}
 
 </script>
