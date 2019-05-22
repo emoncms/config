088a0e350af56d810a1f31df3e5f05ba7d134b67
Emrys <emrys@openenergymonitor.org>
2019-05-22 10:29:51 +0100

    added example of how config menu is used in sidebar

--- config_menu.php
+++ config_menu.php
@@ -4,6 +4,7 @@
         'text' => _("Emonhub"),
         'path' => 'config',
         'icon' => 'bullhorn',
+        'active' => 'config',
         'data' => array(
             'sidebar' => '#sidebar_config'
         )
@@ -16,3 +17,22 @@
         array('text'=>_('EmonHub.Conf Editor'), 'path'=>'config/editor'),
         array('text'=>_('View Log'), 'path'=>'config#log')
     );
+
+// UNCOMMENT THE FOLLOWING TO PUT THE CONFIG MENU ITEMS IN THE EMONCMS SIDEBAR
+// ---------------------------------------------------------------------------
+    // $menu['sidebar']['emoncms'][] = array(
+    //     'text' => _("Emonhub"),
+    //     'path' => 'config',
+    //     'active'=>'config',
+    //     'icon' => 'show_chart',
+    //     'order' => 2,
+    //     'li_id' => 'config-link',
+    //     'data'=> array('sidebar' => '#sidebar_config')
+    // );
+
+    // $menu['sidebar']['includes']['emoncms']['config'] = array(
+    //     array('text'=>_('EmonCMS Connect'), 'path'=>'config/connect'),
+    //     array('text'=>_('Calibration'), 'path'=>'config/calibration'),
+    //     array('text'=>_('EmonHub.Conf Editor'), 'path'=>'config/editor'),
+    //     array('text'=>_('View Log'), 'path'=>'config#log')
+    // );
